73486533a034a10dea55819b698ca33d2b8a62cc
Nadim Mahmud <mdnadim4@gmail.com>
2021-10-27 15:36:50 +0600

    Fixture Script Completed

--- cypress/integration/practice-2/TC_04.spec.js
+++ cypress/integration/practice-2/TC_04.spec.js
@@ -2,14 +2,26 @@
 
 describe('Handling Table Element', () => {
 
-    beforeEach(() => {
-        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+    before(() => {
+        cy.fixture('example').then((data) => {
+            globalThis.data = data
+        })
     })
 
 
-    it('Verify Table data', () => {
+    it('Fixure & Custom Commands', () => {
         
-        cy.get('tbody>tr>th').should('have.length', 3)
+        cy.visit('https://rahulshettyacademy.com/angularpractice')
+
+        cy.get('[name="name"]:nth-child(2)').type(this.data.name).should('have.value', this.data.name)
+        cy.get('[name="email"]').type(this.data.email).should('have.value', this.data.email)
+        cy.get('[type="password"]').type(this.data.password).should('have.value', this.data.password)
+        cy.get('[type="checkbox"]').check().should('be.checked')
+        cy.get('[id="exampleFormControlSelect1"]').select(this.data.gender).should('have.value', this.data.gender)
+        cy.get('[[type="radio"]]').check(this.data.status).should('be.checked')
+        cy.get('[id="inlineRadio3"]').should('be.disabled')
+        cy.get('[type="date"]').type(this.data.date)
+        cy.get('[type="submit"]').click()
 
     });
 
